83c21059e65566efc7c13a9b1d6829632fd97396
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2024-05-30 23:23:27 -0400

    adds struct employee
    NOTES:
    - adds basic employee info
    - adds memory (de)allocation
    - does not check for malloc errors for simplicity at this point
    - displays the employee info
    - valgrind reports no issues

---
+++ src/roster/Roster.cpp
@@ -0,0 +1,119 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+struct Employee {
+	char *id;
+	char *firstName;
+	char *lastName;
+	char *phoneNumber;
+	char *position;
+	double *basicSalary;
+	Employee(char *id,
+		 char *firstName,
+		 char *lastName,
+		 char *phoneNumber,
+		 char *position,
+		 double *basicSalary);
+	void *operator new(size_t size);
+	void operator delete(void *p);
+	void disp() const;
+};
+
+int main ()
+{
+	char *id = strdup("1234-5678-9ABC-DEFG");
+	char *firstName = strdup("First");
+	char *lastName = strdup("Last");
+	char *phoneNumber = strdup("999-999-9999");
+	char *position = strdup("position");
+	double *basicSalary = (double*) malloc(sizeof(double));
+	*basicSalary = 1.0;
+	Employee *employee = new Employee(id,
+					  firstName,
+					  lastName,
+					  phoneNumber,
+					  position,
+					  basicSalary);
+	employee->disp();
+	delete(employee);
+	employee = NULL;
+	return 0;
+}
+
+Employee::Employee(char *id,
+		   char *firstName,
+		   char *lastName,
+		   char *phoneNumber,
+		   char *position,
+		   double *basicSalary)
+{
+	this->id = id;
+	this->firstName = firstName;
+	this->lastName = lastName;
+	this->phoneNumber = phoneNumber;
+	this->position = position;
+	this->basicSalary = basicSalary;
+}
+
+void *Employee::operator new(size_t size)
+{
+	return malloc(size);
+}
+
+void Employee::operator delete(void *vp)
+{
+	Employee *p = (Employee*) vp;
+	free(p->id);
+	free(p->firstName);
+	free(p->lastName);
+	free(p->phoneNumber);
+	free(p->position);
+	free(p->basicSalary);
+	p->id = NULL;
+	p->firstName = NULL;
+	p->lastName = NULL;
+	p->phoneNumber = NULL;
+	p->position = NULL;
+	p->basicSalary = NULL;
+	free(p);
+	p = NULL;
+	vp = NULL;
+}
+
+void Employee::disp() const
+{
+	printf("id: %s\n", this->id);
+	printf("firstName: %s\n", this->firstName);
+	printf("lastName: %s\n", this->lastName);
+	printf("phoneNumber: %s\n", this->phoneNumber);
+	printf("position: %s\n", this->position);
+	printf("basicSalary: %lf\n", *this->basicSalary);
+}
+
+/*
+
+Rosters						May 30, 2024
+
+source: Roster.cpp
+author: @misael-diaz
+
+Copyright (C) 2024 Misael Díaz-Maldonado
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+References:
+
+[0] https://en.cppreference.com/w/cpp
+
+*/
